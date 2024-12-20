1af7fb372a8e145bd3567ff851d3ef703ffc152a
- <-@>
2024-12-20 11:39:55 -0700

    -templating

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,39 @@ import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
 import org.dreambot.api.utilities.Logger;
 
-@ScriptManifest(name = "Script Name", description = "My script description!", author = "Developer Name",
-        version = 1.0, category = Category.WOODCUTTING, image = "")
+@ScriptManifest(name = "Specter", description = "Specter Automation presents...", author = "Specter",
+        version = 1.00, category = Category.MONEYMAKING, image = "")
 public class Main extends AbstractScript {
 
+    //-------ON SCRIPT START-------//
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    //-------MAIN LOOP-------//
     @Override
     public int onLoop() {
         return 0;
+
+    }
+
+    //-------ON SCRIPT EXIT-------//
+    @Override
+    public void onExit() {
+        super.onExit();
+    }
+
+    //-------ON SCRIPT PAUSED-------//
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    //-------ON SCRIPT RESUMED-------//
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 
 }
